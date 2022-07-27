d05c7b783fa9305c311411e11503c64d4fc8b46f
Emily Sheehan <69002400+emilyws27@users.noreply.github.com>
2022-07-27 09:50:29 -0400

    Time: 133 ms (39.18%), Space: 15.1 MB (50.46%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict = {}
-        for index, num in enumerate(nums):
-            sum = target - num
-            if sum in dict:
-                return [nums.index(sum), index]
-            else:
-                dict[num] = True
+    def twoSum(self, arr: List[int], target_sum: int) -> List[int]:
+        seen = {}
+        for index, val in enumerate(arr):
+            remaining = target_sum - val
+            if remaining in seen:
+                return [seen[remaining], index]
+            seen[val] = index
         return []
