e4af4ab4ad8aa27431826e4e60145e1fff9b77b2
Emily Sheehan <69002400+emilyws27@users.noreply.github.com>
2022-09-18 10:39:37 +0200

    Time: 146 ms (5.59%), Space: 14 MB (36.02%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -2,6 +2,9 @@ class Solution:
     def arraySign(self, nums: List[int]) -> int:
         prod = 1
         
+        if 0 in nums:
+            return 0
+        
         for num in nums:
             prod = prod * num
             
@@ -9,8 +12,7 @@ class Solution:
             return 1
         elif prod < 0:
             return -1
-        else:
-            return 0
+       
         
         
             
