e82d0d3016c4c2c7b47cd9c32a801cd3b8275839
Emily Sheehan <69002400+emilyws27@users.noreply.github.com>
2022-09-20 17:14:03 +0200

    Time: 87 ms (50.72%), Space: 13.9 MB (73.50%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -1,20 +1,6 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        # place = 1
-        # numOne = 0 
-        # numTwo = 0
-        # d = {
-        #     '1': 1,
-        #     '2': 2,
-        #     '3': 3,
-        #     '4': 4,
-        #     '5': 5,
-        #     '6': 6,
-        #     '7': 7,
-        #     '8': 8,
-        #     '9': 9,
-        #     '0': 0
-        # }
+
         if num1 == '0' or num2 == '0':
             return '0'
         
@@ -28,8 +14,5 @@ class Solution:
             int2 += 10 ** i * digits[n]
             
         return str(int1 * int2)
-#         for i in range(len(num1.reverse())):
-#             numOne += d[num1[i]] * place
-#             numTwo += d[num2[i]] * place
-#             place = place * 10
+      
             
