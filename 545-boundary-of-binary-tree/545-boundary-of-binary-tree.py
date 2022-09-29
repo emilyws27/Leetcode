5f13aaac1e22a3e28a336689ff042230f303a51d
Emily Sheehan <69002400+emilyws27@users.noreply.github.com>
2022-09-29 14:24:29 +0200

    Time: 93 ms (24.03%), Space: 15.4 MB (95.77%) - LeetHub

---
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return []
+
+        left_bd_nodes = [root]
+        cur = root.left
+        while cur:
+            left_bd_nodes.append(cur)
+            cur = cur.left or cur.right
+
+        right_bd_nodes = [root]
+        cur = root.right
+        while cur:
+            right_bd_nodes.append(cur)
+            cur = cur.right or cur.left
+
+        leaf_nodes = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+            if not node.left and not node.right:
+                leaf_nodes.append(node)
+
+        ans = []
+        seen = set()
+        def visit(node):
+            if node not in seen:
+                seen.add(node)
+                ans.append(node.val)
+
+        for node in left_bd_nodes: visit(node)
+        for node in leaf_nodes: visit(node)
+        for node in reversed(right_bd_nodes): visit(node)
+
+        return ans
